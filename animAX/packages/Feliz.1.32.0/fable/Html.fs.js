b68018c33f4b57192cfa99d9c8df49b625b19cce
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 02:39:13 +0700

    delete all

--- animAX/packages/Feliz.1.32.0/fable/Html.fs.js
+++
@@ -1,9 +0,0 @@
-import { createElement } from "react";
-import { reactApi } from "./Interop.fs.js";
-
-export function Html_h1_C6540BC(children) {
-    return createElement("h1", {
-        children: reactApi.Children.toArray(Array.from(children)),
-    });
-}
-
