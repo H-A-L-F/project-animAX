64e9c0d29b2565599a647add8034057b77a7857f
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 13:54:29 +0700

    Anime Model

---
+++ WebApp-animAX/WebApp-animAX/Models/Anime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_animAX.Models
+{
+    public class Anime
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int subscriptionPrice { get; set; }
+    }
+}
