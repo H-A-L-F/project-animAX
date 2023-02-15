e7352e14a678017b7234dbaa4110b53d7435949b
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 14:52:53 +0700

    Create AnimeFactory.cs

---
+++ WebService-animAX/Factory/AnimeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_animAX.Factory
+{
+    public class AnimeFactory
+    {
+        public static void Create(string title, string price)
+        {
+            ServiceDatabaseEntities db = new ServiceDatabaseEntities();
+            Anime anime = new Anime();
+            anime.Title = title;
+            anime.SubscriptionPrice = int.Parse(price);
+            db.Animes.Add(anime);
+            db.SaveChanges();
+        }
+    }
+}
