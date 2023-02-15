b259905b0a3e0accc24663125f78dfc07fdaf510
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 14:42:04 +0700

    Create AnimeHandler.cs

---
+++ WebService-animAX/Handler/AnimeHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_animAX.Handler
+{
+    public class AnimeHandler
+    {
+        public static Anime Show(int id)
+        {
+            return AnimeRepository.Show(id);
+        }
+        public static void create(string title, string price)
+        {
+            AnimeRepository.create(title, price);
+        }
+
+        public static void update(int id, string title, string price)
+        {
+            AnimeRepository.update(id, title, price);
+        }
+
+        public static bool remove(int id)
+        {
+            return AnimeRepository.remove(id);
+        }
+
+        public static List<Anime> get()
+        {
+            return AnimeRepository.get();
+        }
+    }
+}
