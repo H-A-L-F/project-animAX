ae689d6f1360350a5b0024635c765ca36840f369
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 14:05:17 +0700

    Create Cart.cs

---
+++ WebApp-animAX/WebApp-animAX/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_animAX.Models
+{
+    public class Cart
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int gameId { get; set; }
+
+        public virtual Anime anime { get; set; }
+        public virtual User user { get; set; }
+    }
+}
