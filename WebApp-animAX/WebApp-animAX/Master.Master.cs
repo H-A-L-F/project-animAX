388ed16630321f977b4ce1e86cb98d1872cfca2c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 07:52:46 +0700

    working home and master

---
+++ WebApp-animAX/WebApp-animAX/Master.Master.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp_animAX
+{
+    public partial class Master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void homeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void registerBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void manageBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cartBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void transactionBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void logoutBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
