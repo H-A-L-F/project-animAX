44b9f32f66308bf9a3a1517fb58b84b54e08db15
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 18:52:48 +0700

    Update ManageUser.aspx.cs

--- WebApp-animAX/WebApp-animAX/Blocs/ManageUser.aspx.cs
+++ WebApp-animAX/WebApp-animAX/Blocs/ManageUser.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebApp_animAX.Modules;
+using WebApp_animAX.Models;
+using Newtonsoft.Json;
 
 namespace WebApp_animAX.Blocs
 {
@@ -14,17 +16,36 @@ namespace WebApp_animAX.Blocs
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            CheckAdmin();
+            ws = new localhost.WebService();
+            string res = ws.getUserList();
+            List<User> userList = JsonConvert.DeserializeObject<List<User>>(res);
+            userGv.DataSource = userList;
+            userGv.DataBind();
         }
 
         protected void btnUpdate_Click(object sender, EventArgs e)
         {
+            string username = tbUpdateUname.Text.Trim();
+            string password = tbUpdatePassword.Text.Trim();
+            string id = tbUpdateUid.Text.Trim();
+            string role = tbUpdateRole.Text.Trim();
+            string res = ws.updateUser(id, username, role, password);
 
+            if (res != null)
+            {
+                lbUpdateError.Text = res;
+            }
+            else
+            {
+                RefreshPage();
+            }
         }
 
         protected void btnDelete_Click(object sender, EventArgs e)
         {
-
+            string id = tbDeleteUid.Text.Trim();
+            ws.removeUser(id);
         }
 
         private void CheckAdmin()
@@ -34,6 +55,11 @@ namespace WebApp_animAX.Blocs
             {
                 Response.Redirect("Login.aspx");
             }
+        }
+
+        private void RefreshPage()
+        {
+            Response.Redirect("ManageUser.aspx");
         }
     }
 }
