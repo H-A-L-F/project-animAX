e9d084977e533af287f1f03cce2bc45798afce64
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 13:58:09 +0700

    Create TransactionHeader.cs

---
+++ WebApp-animAX/WebApp-animAX/Models/TransactionHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_animAX.Models
+{
+    public class TransactionHeader
+    {
+        public int id { get; set; }
+        public DateTime date { get; set; }
+        public int userId { get; set; }
+
+        public virtual ICollection<TransactionDetail> transactionDetails { get; set; }
+        public virtual User user { get; set; }
+    }
+}
