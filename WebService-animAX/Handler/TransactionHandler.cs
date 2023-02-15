97ed1b30483123a897041b7f6c62e1fa82d23624
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 16:02:41 +0700

    handler

---
+++ WebService-animAX/Handler/TransactionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_animAX.Handler
+{
+    public class TransactionHandler
+    {
+        public static TransactionHeader InsertHeader(int uid)
+        {
+            return TransactionRepository.InsertHeader(uid);
+        }
+
+        public static List<TransactionHeader> GetHeader(int uid)
+        {
+            return TransactionRepository.GetHeader(uid);
+        }
+
+        public static List<TransactionDetail> GetDetail(int tid)
+        {
+            return TransactionRepository.GetDetail(tid);
+        }
+
+        public static void InsertDetail(int tid, int aid)
+        {
+            TransactionRepository.InsertDetail(tid, aid);
+        }
+    }
+}
