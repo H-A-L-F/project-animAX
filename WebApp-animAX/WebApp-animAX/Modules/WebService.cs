1c746008e2699112b34d28680fe9b47aa490c93f
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 20:18:39 +0700

    WebService Singleton

---
+++ WebApp-animAX/WebApp-animAX/Modules/WebService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_animAX.Modules
+{
+    public class WebService
+    {
+        private static WebService instance;
+
+        private WebService()
+        {
+
+        }
+
+        public static WebService getInstance()
+        {
+            if (instance == null) instance = new WebService();
+            return instance;
+        }
+    }
+}
