95f30ece665e3c4f7adb4b869a8b8417dd7f5a4a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 16:46:30 +0700

    factory

---
+++ WebService-animAX/Factory/TransactionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_animAX.Factory
+{
+    public class TransactionFactory
+    {
+        public static TransactionHeader CreateHeader(int uid)
+        {
+            ServiceDatabaseEntities db = new ServiceDatabaseEntities();
+            TransactionHeader transaction = new TransactionHeader();
+            transaction.UserId = uid;
+            transaction.Date = DateTime.Now;
+
+            db.TransactionHeaders.Add(transaction);
+            db.SaveChanges();
+
+            return transaction;
+        }
+
+        public static void CreateDetail(int tid, int aid)
+        {
+            ServiceDatabaseEntities db = new ServiceDatabaseEntities();
+            TransactionDetail detail = new TransactionDetail();
+            detail.AnimeId = aid;
+            detail.TransactionId = tid;
+            db.TransactionDetails.Add(detail);
+            db.SaveChanges();
+        }
+    }
+}
--- WebService-animAX/Repository/TransactionRepository.cs
+++ WebService-animAX/Repository/TransactionRepository.cs
@@ -7,5 +7,28 @@ namespace WebService_animAX.Repository
 {
     public class TransactionRepository
     {
+        public static TransactionHeader InsertHeader(int uid)
+        {
+            return TransactionFactory.CreateHeader(uid);
+        }
+        public static List<TransactionHeader> GetHeader(int uid)
+        {
+
+            ServiceDatabaseEntities db = new ServiceDatabaseEntities();
+            List<TransactionHeader> headerList = (from data in db.TransactionHeaders select data).Where(c => c.Id == uid).ToList<TransactionHeader>();
+            return headerList;
+        }
+
+        public static List<TransactionDetail> GetDetail(int tid)
+        {
+            ServiceDatabaseEntities db = new ServiceDatabaseEntities();
+            List<TransactionDetail> detailList = (from data in db.TransactionDetails select data).Where(c => c.TransactionId == tid).ToList<TransactionDetail>();
+            return detailList;
+        }
+
+        public static void InsertDetail(int headerId, int gameId)
+        {
+            TransactionFactory.CreateDetail(headerId, gameId);
+        }
     }
 }
