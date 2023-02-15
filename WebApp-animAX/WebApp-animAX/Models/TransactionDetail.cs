a6af6875ab89f5678f3a459ac5e3f568fe3e975c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 14:00:16 +0700

    Create TransactionDetail.cs

---
+++ WebApp-animAX/WebApp-animAX/Models/TransactionDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_animAX.Models
+{
+    public class TransactionDetail
+    {
+        public int transactionId { get; set; }
+        public int gameId { get; set; }
+        public int quantity { get; set; }
+
+        public virtual Anime anime { get; set; }
+        public virtual TransactionHeader transactionHeader { get; set; }
+    }
+}
