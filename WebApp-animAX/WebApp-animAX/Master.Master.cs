72d7bc61289c0761fbf34757249ba7851a349c2d
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 08:02:49 +0700

    routing

--- WebApp-animAX/WebApp-animAX/Master.Master.cs
+++ WebApp-animAX/WebApp-animAX/Master.Master.cs
@@ -16,32 +16,32 @@ namespace WebApp_animAX
 
         protected void homeBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/Home.aspx");
         }
 
         protected void loginBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/Login.aspx");
         }
 
         protected void registerBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/Register.aspx");
         }
 
         protected void manageBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/ManageUser.aspx");
         }
 
         protected void cartBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/Cart.aspx");
         }
 
         protected void transactionBtn_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("./Blocs/Subscriptions.aspx");
         }
 
         protected void logoutBtn_Click(object sender, EventArgs e)
