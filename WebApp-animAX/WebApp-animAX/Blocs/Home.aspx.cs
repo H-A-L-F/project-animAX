46402923594d1536b1c99e91a6979dfb97eb8f13
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-15 19:05:37 +0700

    Home done

--- WebApp-animAX/WebApp-animAX/Blocs/Home.aspx.cs
+++ WebApp-animAX/WebApp-animAX/Blocs/Home.aspx.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApp_animAX.Models;
+using Newtonsoft.Json;
+using WebApp_animAX.Modules;
 
 namespace WebApp_animAX.Blocs
 {
     public partial class Home : System.Web.UI.Page
     {
+        private localhost.WebService ws;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            InitiateData();
+            CheckAdminView();
+            CheckUserView();
+            loadAnimeData();
         }
 
         protected void addCart_Click(object sender, EventArgs e)
@@ -21,17 +28,94 @@ namespace WebApp_animAX.Blocs
 
         protected void createBtn_Click(object sender, EventArgs e)
         {
+            string name = tbInsertAnimeName.Text.Trim();
+            string price = tbInsertSubscriptionPrice.Text.Trim();
 
+            string res = ws.createAnime(name, price);
+            if (res != "")
+            {
+                lbInsertError.Text = res;
+            }
+            else
+            {
+                RefreshPage();
+            }
         }
 
         protected void updateBtn_Click(object sender, EventArgs e)
         {
+            string id = tbAnimeUpdateId.Text.Trim();
+            string name = tbUpdateAnimeTitle.Text.Trim();
+            string price = tbUpdateSubscriptionPrice.Text.Trim();
 
+            string res = ws.updateAnime(int.Parse(id), name, price);
+            if (res != "")
+            {
+                lbUpdateError.Text = res;
+            }
+            else
+            {
+                RefreshPage();
+            }
         }
 
         protected void deleteBtn_Click(object sender, EventArgs e)
         {
+            string id = tbDeleteAnimeId.Text.Trim();
+            bool res = ws.removeGame(int.Parse(id));
+            if (res)
+            {
+                RefreshPage();
+            }
+            else
+            {
+                lbDeleteError.Text = "Error Deleting";
+            }
+        }
 
+        private void loadAnimeData()
+        {
+            string res = ws.getAnime();
+            if (res != null)
+            {
+                List<Anime> gameList = JsonConvert.DeserializeObject<List<Anime>>(res);
+                animeGv.DataSource = gameList;
+                animeGv.DataBind();
+            }
+        }
+
+        private void InitiateData()
+        {
+            ws = new localhost.WebService();
+        }
+
+        private void CheckAdminView()
+        {
+            if (UserSession.getInstance().GetRole(Request).Equals("Admin"))
+            {
+                adminView.Visible = true;
+            }
+            else
+            {
+                adminView.Visible = false;
+            }
+        }
+
+        private void CheckUserView()
+        {
+            if (UserSession.getInstance().GetRole(Request).Equals(""))
+            {
+
+                cartView.Visible = false;
+            }
+            else
+            {
+                cartView.Visible = true;
+            }
+        }
+        private void RefreshPage()
+        {
+            Response.Redirect("Home.aspx");
         }
     }
 }
